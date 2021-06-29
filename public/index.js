ee9c3b3f5254b3018c382c17fe4be7dee28deb3b
Arkenite03 <vishwa.sharma30@gmail.com>
2021-06-29 12:39:26 +0530

    Positioned Video Containers and Disconnetion on leaving

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
 const socket = io('/');
 const peer = new Peer();
 const videoDiv = document.getElementById('videoDiv');
+const myVideo = document.getElementById('myVideo');
+
+let otherUsers = {}, otherVid = {};
 
 peer.on('open' , (id)=>{
     socket.emit("newUser" , id);
@@ -13,11 +16,19 @@ navigator.mediaDevices.getUserMedia({
 
     const myVid = document.createElement('video');
     myVid.muted = true;
-    addVideo(myVid, stream);
+    myVid.srcObject = stream;
+    myVid.addEventListener('loadedmetadata', () => {
+      myVid.play()
+    })
+    myVideo.append(myVid);
 
     peer.on('call', callObj => {
         callObj.answer(stream);
+        otherUsers[callObj.peer] = callObj;
         const newUserVid = document.createElement('video');
+        const newDisconnect = document.createElement('p');
+        newDisconnect.innerText = "Disconnecting...";
+        otherVid[callObj.peer] = [newUserVid, newDisconnect];
         callObj.on('stream', userStream => {
             addVideo(newUserVid, userStream);
         })
@@ -25,7 +36,11 @@ navigator.mediaDevices.getUserMedia({
     
     socket.on('userJoined', (userid) =>{
         const call = peer.call(userid, stream);
+        otherUsers[userid] = call;
         const newUserVid = document.createElement('video');
+        const newDisconnect = document.createElement('p');
+        newDisconnect.innerText = "Disconnecting...";
+        otherVid[userid] = [newUserVid, newDisconnect];
         call.on('stream', userStream => {
             addVideo(newUserVid, userStream);
         })
@@ -34,6 +49,16 @@ navigator.mediaDevices.getUserMedia({
   }).catch(err=>{
       alert(err.message)
   })
+
+  socket.on('userDisconnected', (userId) => {
+    if(otherUsers[userId]){
+      otherUsers[userId].close();
+      otherVid[userId][0].parentNode.replaceChild(otherVid[userId][1], otherVid[userId][0]);
+      setTimeout(() => {
+        otherVid[userId][1].remove();
+      }, 1500)
+    }
+  });
 
   function addVideo(vidElement, stream){
     vidElement.srcObject = stream;
--- server.js
+++ server.js
@@ -6,18 +6,24 @@ const io = require('socket.io')(server);
 app.set("view engine", "ejs"); 
   
 app.use(express.static("public")); 
-  
 
 app.get('/' , (req,res)=>{
+    res.render('main');
+});
+
+app.get('/home' , (req,res)=>{
     res.render('index');
 });
 
 io.on('connection' , (socket)=>{
     socket.on('newUser' , (id)=>{
-        socket.join('/');
-        socket.broadcast.emit("userJoined" , id);   
+        // socket.join('/home');
+        socket.broadcast.emit("userJoined" , id);
+        socket.on('disconnect', () => {
+            socket.broadcast.emit('userDisconnected', id);
+        }); 
     });
-});
+}); 
   
 server.listen(4000 , ()=>{
     console.log("Server running on port 4000");
