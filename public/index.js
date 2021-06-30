c89e216e63498f7a6e82038a64ca31311f1a9e7a
Arkenite03 <vishwa.sharma30@gmail.com>
2021-06-30 16:41:48 +0530

    Back

--- public/index.js
+++ public/index.js
@@ -1,11 +1,14 @@
 const socket = io('/');
 const peer = new Peer();
 const videoDiv = document.getElementById('videoDiv');
+<<<<<<< HEAD
 const myVideo = document.getElementById('myVideo');
 const nameContainer = document.getElementById('names');
 let guserName;
 
 let otherUsers = {}, otherVid = {};
+=======
+>>>>>>> parent of ee9c3b3 (Positioned Video Containers and Disconnetion on leaving)
 
 peer.on('open' , (id)=>{
     socket.emit("newUser" , id);
@@ -18,20 +21,12 @@ navigator.mediaDevices.getUserMedia({
 
     const myVid = document.createElement('video');
     myVid.muted = true;
-    myVid.srcObject = stream;
-    myVid.addEventListener('loadedmetadata', () => {
-      myVid.play()
-    })
-    myVideo.append(myVid);
+    addVideo(myVid, stream);
 
     peer.on('call', callObj => {
         nameContainer.innerText = guserName;
         callObj.answer(stream);
-        otherUsers[callObj.peer] = callObj;
         const newUserVid = document.createElement('video');
-        const newDisconnect = document.createElement('p');
-        newDisconnect.innerText = "Disconnecting...";
-        otherVid[callObj.peer] = [newUserVid, newDisconnect];
         callObj.on('stream', userStream => {
             addVideo(newUserVid, userStream);
         })
@@ -41,11 +36,7 @@ navigator.mediaDevices.getUserMedia({
         nameContainer.innerText = userName;
         guserName = userName;
         const call = peer.call(userid, stream);
-        otherUsers[userid] = call;
         const newUserVid = document.createElement('video');
-        const newDisconnect = document.createElement('p');
-        newDisconnect.innerText = "Disconnecting...";
-        otherVid[userid] = [newUserVid, newDisconnect];
         call.on('stream', userStream => {
             addVideo(newUserVid, userStream);
         })
@@ -54,16 +45,6 @@ navigator.mediaDevices.getUserMedia({
   }).catch(err=>{
       alert(err.message)
   })
-
-  socket.on('userDisconnected', (userId) => {
-    if(otherUsers[userId]){
-      otherUsers[userId].close();
-      otherVid[userId][0].parentNode.replaceChild(otherVid[userId][1], otherVid[userId][0]);
-      setTimeout(() => {
-        otherVid[userId][1].remove();
-      }, 1500)
-    }
-  });
 
   function addVideo(vidElement, stream){
     vidElement.srcObject = stream;
--- server.js
+++ server.js
@@ -8,13 +8,17 @@ let userName = "";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs"); 
 app.use(express.static("public")); 
+  
 
 app.get('/' , (req,res)=>{
+<<<<<<< HEAD
     res.render('main');
 });
 
 app.post('/home', function (req, res) {
     // res.render('index', { name: req.body.name });
+=======
+>>>>>>> parent of ee9c3b3 (Positioned Video Containers and Disconnetion on leaving)
     res.render('index');
     userName = req.body.name;
     console.log(req.body.name);
@@ -28,13 +32,18 @@ app.post('/home', function (req, res) {
 
 io.on('connection' , (socket)=>{
     socket.on('newUser' , (id)=>{
+<<<<<<< HEAD
         // socket.join('/home');
         socket.broadcast.emit("userJoined" , id, userName);
         socket.on('disconnect', () => {
             socket.broadcast.emit('userDisconnected', id);
         }); 
+=======
+        socket.join('/');
+        socket.broadcast.emit("userJoined" , id);   
+>>>>>>> parent of ee9c3b3 (Positioned Video Containers and Disconnetion on leaving)
     });
-}); 
+});
   
 server.listen(4000 , ()=>{
     console.log("Server running on port 4000");
