5c8ca7948d67c649faee025f859e7cfb03fcbc70
Arkenite03 <vishwa.sharma30@gmail.com>
2021-07-02 23:42:55 +0530

    Again Name Final(Branching Issue)

--- server.js
+++ server.js
@@ -18,16 +18,11 @@ app.get('/room' , (req,res)=>{
 });
 
 io.on('connection' , (socket)=>{
-<<<<<<< HEAD
-    socket.on('newUser' , (id)=>{
-        socket.broadcast.emit("userJoined" , id);
-=======
     socket.on('newUser' , (id, name) => {
         socket.join('/room');
         socket.emit('allNames', otherNames);
         otherNames[id] = name;
         socket.broadcast.emit("userJoined" , id, name);
->>>>>>> prototype
         socket.on('disconnect', () => {
             socket.broadcast.emit('userDisconnected', id);
         }); 
